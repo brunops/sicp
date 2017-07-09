c092d056598c8d1c841617b55fddeeeaa577d88b
bsanches <bsanches@paypal.com>
2017-07-09 11:59:09 -0700

    Conform all solution files to the same format

--- chapter-1/ex-1.4.scm
+++ chapter-1/ex-1.4.scm
@@ -7,9 +7,9 @@
 (define (a-plus-abs-b a b)
   ((if (> b 0) + -) a b))
 
-(a-plus-abs-b 1 2) ; -> 3
+(a-plus-abs-b 1 2)   ; -> 3
 (a-plus-abs-b -1 -2) ; -> 1
-(a-plus-abs-b 1 -2) ; -> 3
-(a-plus-abs-b -1 2) ; -> 1
+(a-plus-abs-b 1 -2)  ; -> 3
+(a-plus-abs-b -1 2)  ; -> 1
 
 ; Answer: The behavior will be a plus |b|
